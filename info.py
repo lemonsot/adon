a9c6c1b1700014e0c024090dfd776aabd165936f
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2023-04-18 13:02:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Gowthaman:gowthaman2008@cluster0.ounmfkw.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Gowthaman")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
